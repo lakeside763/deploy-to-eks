6f96990818acb136fb7f62262782d3dbb693beb4
lakeside763 <lakeside763@gmail.com>
2024-01-20 09:14:20 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,7 @@ $(aws ecr get-login --no-include-email --region ${AWS_REGION})
 # SHA8=$(echo ${GITHUB_SHA} | cut -c1-8)
 
 # Build, tag, and push image to ECR
-docker build -f /workspace/src/Dockerfile -t ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG} /workspace/src
+docker build -f /workspace/Dockerfile -t ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG} /workspace
 docker push ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}
 
 # # Deploy to EKS with Helm
